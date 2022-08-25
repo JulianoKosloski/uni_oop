464d3a9c65fd3eb5b980042ca63d0a04d7a7d2cb
JulianoKosloski <juliano.koslos@gmail.com>
2022-08-25 16:15:48 -0300

    testing

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String x = "Hello World!";
+        String y = "Don't talk to me";
+        // this is a java comment
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(x);
+            } else {
+                System.out.println(y);
+            }
+        }
+
+    }
+}
