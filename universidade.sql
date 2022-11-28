5b090376f8fde825e19d7e917bf0e139bc0ad7cb
JulianoKosloski <juliano.koslos@gmail.com>
2022-11-28 10:29:51 -0300

    testing sql

--- universidade.sql
+++ universidade.sql
@@ -33,3 +33,25 @@ CREATE TABLE alunos (
     PRIMARY KEY (id_aluno),
     FOREIGN KEY (id_curso) REFERENCES cursos(id_curso)
 );
+
+
+INSERT INTO cursos VALUES 
+            (1, "Biologia", "Ana Grizz"), 
+            (2, "Letras", "Cristina Polera"), 
+            (3, "Engenharia Eletrica", "pablo Tisseau"), 
+            (4, "Artes Visuais", "Daniel Fabris");
+
+INSERT INTO disciplinas VALUES 
+            (1, "Sapologia", 30, "Altos sapos"), 
+            (2, "Sapologia", 30, "Altos sapos"), 
+            (3, "Sapologia", 30, "Altos sapos"),
+            (4, "Sapologia", 30, "Altos sapos"),
+            (5, "Sapologia", 30, "Altos sapos"), 
+            (6, "Sapologia", 30, "Altos sapos");
+
+INSERT INTO alunos VALUES 
+            (1, "Juliano", 23213213, 1), 
+            (2, "Pedro", 252144213, 2), 
+            (3, "Ana", 2329604323, 2),
+            (4, "Beatriz", 232164323, 3), 
+            (5, "Frederico", 573813213, 4);
